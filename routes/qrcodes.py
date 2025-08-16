881662f453c2fd8d03cdade84b2a0ed26faf7798
Hossein <hosseinyadegarnia1391@gmail.com>
2025-08-16 11:39:06 +0330

    Add dark qrcode feature

--- routes/qrcodes.py
+++ routes/qrcodes.py
@@ -6,12 +6,23 @@ import qrcode
 router = APIRouter()
 
 @router.get("/api/generate-qrcode" , response_class=Response , tags=["Generate qrcode"])
-def generate_qrcode(content: str):
-    qr = qrcode.make(content)
-    pil_image = qr.get_image()
+def generate_qrcode(content: str , dark: int = 0):
+    if dark and dark == 1:
+        qr = qrcode.QRCode(version=1, box_size=10, border=4)
+        qr.add_data(content)
+        qr.make()
+        img = qr.make_image(fill_color="white", back_color="black")
 
-    buffer = io.BytesIO()
-    pil_image.save(buffer, format="PNG")
-    binary_data = buffer.getvalue()
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+        binary_data = buffer.getvalue()
+
+    else : 
+        qr = qrcode.make(content)
+        pil_image = qr.get_image()
+
+        buffer = io.BytesIO()
+        pil_image.save(buffer, format="PNG")
+        binary_data = buffer.getvalue()
 
     return Response(content=binary_data , media_type="image/png")
